c92f91cd2d191686959b88aa2eecc407b2706aea
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-06 10:40:53 +0530

    Displaying welcome message in flip coin simulation program

---
+++ FlipCoinSimulation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+#
+#Welcome to flip coin simulation Program on Master #Branch
+#
+#
+
+
+echo ""
+echo "Welcome to Flip Coin Simulation Program "
+echo "This problem displays the winner Heads or Tails"
+echo ""
