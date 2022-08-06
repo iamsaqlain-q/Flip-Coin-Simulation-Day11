dd10e14fa029b629770a1c8279efbadbc73b5afa
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-06 10:46:49 +0530

    Flipping coin to display head and tail as a winner

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,10 +2,20 @@
 #
 #Welcome to flip coin simulation Program on Master #Branch
 #
-#
-
 
 echo ""
 echo "Welcome to Flip Coin Simulation Program "
 echo "This problem displays the winner Heads or Tails"
+echo ""
+
+
+#As a Simulator, start with Flipping a Coin to Display Heads or Tails as winner
+ 
+flip=$((RANDOM%2))
+
+if [[ ${flip} -eq 0 ]]; then
+	echo HEADS
+elif [[ ${flip} -eq 1 ]]; then
+	echo TAILS
+fi
 echo ""
